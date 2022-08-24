6b9f1e4bdc3de42cf2d206dfda1fcfc0a67887a3
zhangcy007 <111864833+zhangcy007@users.noreply.github.com>
2022-08-24 16:49:43 +0800

    pull commit

--- src/main/java/github_test.java
+++ src/main/java/github_test.java
@@ -7,5 +7,6 @@ public class github_test {
         System.out.println("master");
         System.out.println("hot-fix");
         System.out.println("push test");
+         System.out.println("pull test");
     }
 }
